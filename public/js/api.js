e997e02ff64d2a515f6da094ee172ff143eafb57
EMSMoreno <107496045+EMSMoreno@users.noreply.github.com>
2023-11-25 11:53:47 +0000

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "d239e4c5030d09dbeee47e5bce87d027";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
